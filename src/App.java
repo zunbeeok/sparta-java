545f1b850be5c8c36d18c399df89a69ccbf7ef65
최준홍 <zunbee@choejunhong-ui-MacBookPro.local>
2024-07-15 13:54:19 +0900

    계산기 Level 1 : 1. Scanner를 사용하여 양의 정수 2개를 전달 받을 수 있습니다.

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요 : ");
+        sc.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요 : ");
+        sc.nextInt();
+
+    }
+}
