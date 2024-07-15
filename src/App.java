a331129d1c5252f609cf633cd2ca9cefa5d98224
최준홍 <zunbee@choejunhong-ui-MacBookPro.local>
2024-07-15 14:19:56 +0900

    계산기 Level 1 : 3. 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력합니다.

--- src/App.java
+++ src/App.java
@@ -12,5 +12,24 @@ public class App {
         System.out.println("사칙연산 기호를 입력하세요 : ");
         char sign = sc.next().charAt(0);
 
+        int result = 0;
+
+        if(sign == '+'){
+            result = x + y;
+        } else if (sign == '-') {
+            result = x - y;
+        }else if (sign == '/'){
+            if (y == 0){
+                System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                return;
+            }
+            result = x / y;
+        }else if(sign == '*'){
+            result = x * y;
+        }else {
+            System.out.println("연산기호가 잘 못 입력되었습니다.");
+        }
+
+        System.out.println("결과: " + result);
     }
 }
