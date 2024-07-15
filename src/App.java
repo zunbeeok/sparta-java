5c1c13f1445c0b47d1b8e7244aa32fc58e9c8ed8
최준홍 <zunbee@choejunhong-ui-MacBookPro.local>
2024-07-15 16:19:08 +0900

    계산기 Level 1 : 7. 연산 결과가 10개로 고정되지 않고 무한이 저장될 수 있도록 소스 코드를 수정합니다.

--- src/App.java
+++ src/App.java
@@ -1,22 +1,21 @@
-import java.sql.Array;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int[] list= new int[10];
-        int index = 0;
+        List<Double> resultList = new ArrayList<>();
 
-        int result = 0;
+        double result = 0;
         boolean con = true;
 
         do{
             System.out.println("첫 번째 숫자를 입력하세요 : ");
-            int x = sc.nextInt();
+            double x = sc.nextInt();
             System.out.println("두 번째 숫자를 입력하세요 : ");
-            int y = sc.nextInt();
+            double y = sc.nextInt();
 
             System.out.println("사칙연산 기호를 입력하세요 : ");
             char sign = sc.next().charAt(0);
@@ -39,20 +38,18 @@ public class App {
             }
 
             System.out.println("결과: " + result);
-            if (index== 10){
-                list[0] = result;
-                index = 0;
-            }else {
-                list[index] = result;
-                index ++;
-            }
+            resultList.add(result);
 
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            if(sc.next().equals("remove")){
+                resultList.removeFirst();
+            }
 
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String answer = sc.next();
 
-            if(answer == "exit"){
+            if(answer.equals("exit")){
                 con = false;
             }
         }while (con );
