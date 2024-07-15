3f7da4e8f063a6441cf9acd0d45f07634b9f184a
최준홍 <zunbee@choejunhong-ui-MacBookPro.local>
2024-07-15 14:44:33 +0900

    계산기 Level 1 : 4. 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.

--- src/App.java
+++ src/App.java
@@ -4,32 +4,44 @@ public class App {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("첫 번째 숫자를 입력하세요 : ");
-        int x = sc.nextInt();
-        System.out.println("두 번째 숫자를 입력하세요 : ");
-        int y = sc.nextInt();
-
-        System.out.println("사칙연산 기호를 입력하세요 : ");
-        char sign = sc.next().charAt(0);
 
         int result = 0;
+        boolean con = true;
 
-        if(sign == '+'){
-            result = x + y;
-        } else if (sign == '-') {
-            result = x - y;
-        }else if (sign == '/'){
-            if (y == 0){
-                System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
-                return;
+        do{
+            System.out.println("첫 번째 숫자를 입력하세요 : ");
+            int x = sc.nextInt();
+            System.out.println("두 번째 숫자를 입력하세요 : ");
+            int y = sc.nextInt();
+
+            System.out.println("사칙연산 기호를 입력하세요 : ");
+            char sign = sc.next().charAt(0);
+
+            if(sign == '+'){
+                result = x + y;
+            } else if (sign == '-') {
+                result = x - y;
+            }else if (sign == '/'){
+                if (y == 0){
+                    System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                    return;
+                }
+                result = x / y;
+            }else if(sign == '*'){
+                result = x * y;
+            }else {
+                System.out.println("연산기호가 잘 못 입력되었습니다.");
             }
-            result = x / y;
-        }else if(sign == '*'){
-            result = x * y;
-        }else {
-            System.out.println("연산기호가 잘 못 입력되었습니다.");
-        }
 
-        System.out.println("결과: " + result);
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String answer = sc.next();
+
+            if(answer == "exit"){
+                con = false;
+            }
+        }while (con );
+
     }
 }
