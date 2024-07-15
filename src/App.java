c9f414bc5eab116c6421e5c5ae0c56201dba5744
최준홍 <zunbee@choejunhong-ui-MacBookPro.local>
2024-07-15 15:11:20 +0900

    계산기 Level 1 : 5.연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.

--- src/App.java
+++ src/App.java
@@ -1,9 +1,13 @@
+import java.sql.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+        int[] list= new int[10];
+        int index = 0;
 
         int result = 0;
         boolean con = true;
@@ -24,16 +28,19 @@ public class App {
             }else if (sign == '/'){
                 if (y == 0){
                     System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
-                    return;
+                    continue;
                 }
                 result = x / y;
             }else if(sign == '*'){
                 result = x * y;
             }else {
                 System.out.println("연산기호가 잘 못 입력되었습니다.");
+                continue;
             }
 
             System.out.println("결과: " + result);
+            list[index] = result;
+            index ++;
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String answer = sc.next();
